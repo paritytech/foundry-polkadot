3dd7c494452e60516b0e5923e6318282968e3ec9
Pavlo Khrystenko <p.khrystenko@gmail.com>
2025-08-22 11:08:18 +0200

    rm useless comments

--- crates/revive-strategy/src/executor/runner.rs
+++ crates/revive-strategy/src/executor/runner.rs
@@ -73,13 +73,6 @@ impl ExecutorStrategyRunner for ReviveExecutorStrategyRunner {
             sp_core::U256::from_little_endian(&amount.as_le_bytes()).min(u128::MAX.into());
         let balance_native =
             BalanceWithDust::<BalanceOf<Runtime>>::from_value::<Runtime>(amount_pvm).unwrap();
-        // let balance = Pallet::<Runtime>::convert_native_to_evm(balance_native);
-        // let amount_evm = U256::from_limbs(balance.0);
-
-        // code below breaks tests
-        // if amount != amount_evm {
-        //     let _ = sh_err!("Amount mismatch {amount} != {amount_evm}, Polkadot balances are
-        // u128. Test results may be incorrect."); }
 
         EvmExecutorStrategyRunner.set_balance(executor, address, amount)?;
 
