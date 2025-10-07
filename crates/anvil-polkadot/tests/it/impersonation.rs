6e34608f4efa99399b2b56ba5da21852b1a3a293
Iulian Barbu <iulian.barbu@parity.io>
2025-10-07 12:23:55 +0000

    anvil-polkadot(tests): parameterize auto_impersonate test with rstest
    Signed-off-by: Iulian Barbu <iulian.barbu@parity.io>

--- crates/anvil-polkadot/tests/it/impersonation.rs
+++ crates/anvil-polkadot/tests/it/impersonation.rs
@@ -10,6 +10,7 @@ use anvil_polkadot::{
 };
 use anvil_rpc::error::ErrorCode;
 use polkadot_sdk::pallet_revive::evm::Account;
+use rstest::rstest;
 use subxt::utils::H160;
 
 // Initialize with some balance a random account and return its address.
@@ -117,8 +118,16 @@ async fn test_impersonate_account() {
 }
 
 #[tokio::test(flavor = "multi_thread")]
-async fn test_auto_impersonate_via_rpc() {
-    let anvil_node_config = AnvilNodeConfig::test_config();
+#[rstest]
+#[case(false)]
+#[case(true)]
+async fn test_auto_impersonate(#[case] rpc_driven: bool) {
+    let mut anvil_node_config = AnvilNodeConfig::test_config();
+    if !rpc_driven {
+        // Enable autoimpersonation via cli.
+        anvil_node_config.enable_auto_impersonate = true;
+    }
+
     let substrate_node_config = SubstrateNodeConfig::new(&anvil_node_config);
     let mut node = TestNode::new(anvil_node_config.clone(), substrate_node_config).await.unwrap();
 
@@ -132,8 +141,12 @@ async fn test_auto_impersonate_via_rpc() {
         transfer_to_unitialized_random_account(&mut node, alith_addr, transfer_amount).await;
 
     // Start impersonating any address now
-    unwrap_response::<()>(node.eth_rpc(EthRequest::AutoImpersonateAccount(true)).await.unwrap())
+    if rpc_driven {
+        unwrap_response::<()>(
+            node.eth_rpc(EthRequest::AutoImpersonateAccount(true)).await.unwrap(),
+        )
         .unwrap();
+    }
 
     // Transfer at block 2.
     let dest_h160 = H160::from_slice(dest_addr.as_slice());
@@ -161,63 +174,6 @@ async fn test_auto_impersonate_via_rpc() {
     );
 
     // Stop impersonating destination, and assert on error when retrying the same transfer.
-    unwrap_response::<()>(node.eth_rpc(EthRequest::AutoImpersonateAccount(false)).await.unwrap())
-        .unwrap();
-    let transaction =
-        TransactionRequest::default().value(transfer_amount).from(dest_addr).to(alith_addr);
-    let err = node.send_transaction(transaction.clone(), None).await.unwrap_err();
-    assert_eq!(err.code, ErrorCode::InternalError);
-    assert_eq!(
-        err.message,
-        format!("Account not found for address {}", dest_addr.to_string().to_lowercase())
-    );
-}
-
-#[tokio::test(flavor = "multi_thread")]
-async fn test_auto_impersonate_via_cli() {
-    let mut anvil_node_config = AnvilNodeConfig::test_config();
-    // Enable autoimpersonation via cli.
-    anvil_node_config.enable_auto_impersonate = true;
-    let substrate_node_config = SubstrateNodeConfig::new(&anvil_node_config);
-    let mut node = TestNode::new(anvil_node_config.clone(), substrate_node_config).await.unwrap();
-
-    // Enable automine.
-    unwrap_response::<()>(node.eth_rpc(EthRequest::SetAutomine(true)).await.unwrap()).unwrap();
-
-    // Create a random account with some balance.
-    let alith_account = Account::from(subxt_signer::eth::dev::alith());
-    let alith_addr = Address::from(ReviveAddress::new(alith_account.address()));
-    let transfer_amount = U256::from(16e17);
-    let dest_addr =
-        transfer_to_unitialized_random_account(&mut node, alith_addr, transfer_amount).await;
-    let dest_h160 = H160::from_slice(dest_addr.as_slice());
-
-    // Auto impersonation enabled previously via cli, so transferring from a random destination
-    // should work.
-    let alith_balance = node.get_balance(alith_account.address(), None).await;
-    let dest_balance = node.get_balance(dest_h160, None).await;
-    let transfer_amount = U256::from(1e11);
-    let transaction =
-        TransactionRequest::default().value(transfer_amount).from(dest_addr).to(alith_addr);
-    let tx_hash = node
-        .send_transaction(transaction, Some(BlockWaitTimeout::new(2, Duration::from_secs(1))))
-        .await
-        .unwrap();
-    tokio::time::sleep(std::time::Duration::from_millis(500)).await;
-    let receipt_info = node.get_transaction_receipt(tx_hash).await;
-
-    // Assert on balances after second transfer.
-    let alith_final_balance = node.get_balance(alith_account.address(), None).await;
-    let dest_final_balance = node.get_balance(dest_h160, None).await;
-    assert_eq!(alith_final_balance, alith_balance + transfer_amount);
-    assert_eq!(
-        dest_final_balance,
-        dest_balance
-            - transfer_amount
-            - AlloyU256::from(receipt_info.effective_gas_price * receipt_info.gas_used).inner()
-    );
-
-    // Stop auto impersonation.
     unwrap_response::<()>(node.eth_rpc(EthRequest::AutoImpersonateAccount(false)).await.unwrap())
         .unwrap();
     let transaction =
