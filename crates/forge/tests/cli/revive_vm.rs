f7145b462e70382d0293b49294eb0e2061d75134
Alexandru Gheorghe <alexandru.gheorghe@parity.io>
2025-10-08 18:41:58 +0300

    remove unneeded changes
    Signed-off-by: Alexandru Gheorghe <alexandru.gheorghe@parity.io>

--- crates/forge/tests/cli/revive_vm.rs
+++ crates/forge/tests/cli/revive_vm.rs
@@ -143,7 +143,15 @@ contract CounterTest is DSTest {
 Compiler run successful!
 [COMPILING_FILES] with [RESOLC_VERSION]
 [RESOLC_VERSION] [ELAPSED]
-Compiler run successful!
+Compiler run successful with warnings:
+Warning: Warning: Your code or one of its dependencies uses the 'extcodesize' instruction, which is
+usually needed in the following cases:
+  1. To detect whether an address belongs to a smart contract.
+  2. To detect whether the deploy code execution has finished.
+Polkadot comes with native account abstraction support (so smart contracts are just accounts
+coverned by code), and you should avoid differentiating between contracts and non-contract
+addresses.
+[FILE]
 
 Ran 4 tests for src/CounterTest.t.sol:CounterTest
 [PASS] testFuzz_SetNumber(uint256) (runs: 256, [AVG_GAS])
