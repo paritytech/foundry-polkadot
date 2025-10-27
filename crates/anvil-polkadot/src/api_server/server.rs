ad1253da051e2fb489abb88c49c13bb06a6e6080
Alexandru Gheorghe <alexandru.gheorghe@parity.io>
2025-10-27 11:06:26 +0200

    fix build
    Signed-off-by: Alexandru Gheorghe <alexandru.gheorghe@parity.io>

--- crates/anvil-polkadot/src/api_server/server.rs
+++ crates/anvil-polkadot/src/api_server/server.rs
@@ -514,7 +514,7 @@ impl ApiServer {
             return Ok(None);
         };
         let block = self.eth_rpc_client.evm_block(block, hydrated_transactions).await;
-        Ok(Some(block))
+        Ok(block)
     }
 
     async fn estimate_gas(
@@ -638,7 +638,7 @@ impl ApiServer {
             return Ok(None);
         };
         let block = self.eth_rpc_client.evm_block(block, hydrated_transactions).await;
-        Ok(Some(block))
+        Ok(block)
     }
 
     pub(crate) async fn snapshot(&mut self) -> Result<U256> {
