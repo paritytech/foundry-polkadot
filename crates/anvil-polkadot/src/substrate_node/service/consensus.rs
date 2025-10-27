30d150312d70698bd7d659f9f315b10670b3b560
Iulian Barbu <14218860+iulianbarbu@users.noreply.github.com>
2025-10-27 11:30:30 +0200

    Update crates/anvil-polkadot/src/substrate_node/service/consensus.rs

--- crates/anvil-polkadot/src/substrate_node/service/consensus.rs
+++ crates/anvil-polkadot/src/substrate_node/service/consensus.rs
@@ -9,7 +9,13 @@ use polkadot_sdk::{
 };
 use std::marker::PhantomData;
 
-/// Consensus data provider for Aura.
+/// Consensus data provider for Aura. This will always use slot 0 (used to determine the
+/// index of the AURA authority from the authorities set by AURA runtimes) for the aura
+/// digest since anvil-polkadot node will be the sole block author and AURA will pick
+/// only its configured address, residing at index 0 in the AURA authorities set. When
+/// forking from an assethub chain, we expect an assethub runtime based on AURA,
+/// which will pick the author based on the slot given through the digest, which will
+/// also result in picking the AURA authority from index 0. 
 pub struct SameSlotConsensusDataProvider<B, P> {
     _phantom: PhantomData<(B, P)>,
 }
