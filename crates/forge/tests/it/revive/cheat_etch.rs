d3bb2b48999e68d90357140c2e54f2b5e66322c7
Alexandru Gheorghe <alexandru.gheorghe@parity.io>
2025-10-28 11:20:16 +0200

    etch with evm code
    Signed-off-by: Alexandru Gheorghe <alexandru.gheorghe@parity.io>

--- crates/forge/tests/it/revive/cheat_etch.rs
+++ crates/forge/tests/it/revive/cheat_etch.rs
@@ -1,19 +1,34 @@
 use crate::{config::*, test_helpers::TEST_DATA_REVIVE};
 use foundry_test_utils::Filter;
+use revive_strategy::ReviveRuntimeMode;
 use revm::primitives::hardfork::SpecId;
+use rstest::rstest;
 
+#[rstest]
+#[case::pvm_mode_with_existing_etched_pvm_code(
+    "testEtchExistingContractPvmCode",
+    ReviveRuntimeMode::Pvm
+)]
+#[case::evm_mode_with_existing_etched_pvm_code(
+    "testEtchExistingContractPvmCode",
+    ReviveRuntimeMode::Evm
+)]
+#[case::pvm_mode_with_existing_etched_evm_code(
+    "testEtchExistingContractPvmCode",
+    ReviveRuntimeMode::Pvm
+)]
+#[case::evm_mode_with_existing_etched_evm_code(
+    "testEtchExistingContractEvmCode",
+    ReviveRuntimeMode::Evm
+)]
+#[case::pvm_mode_with_any_etched_pvm_code("testEtchAnyContractPvmCode", ReviveRuntimeMode::Pvm)]
+#[case::evm_mode_with_any_etched_pvm_code("testEtchAnyContractPvmCode", ReviveRuntimeMode::Evm)]
+#[case::pvm_mode_with_any_etched_evm_code("testEtchAnyContractEvmCode", ReviveRuntimeMode::Pvm)]
+#[case::evm_mode_with_any_etched_evm_code("testEtchAnyContractEvmCode", ReviveRuntimeMode::Evm)]
 #[tokio::test(flavor = "multi_thread")]
-async fn test_etch_existing_contract() {
-    let runner: forge::MultiContractRunner = TEST_DATA_REVIVE.runner_revive();
-    let filter = Filter::new("testEtchExistingContract", "EtchTest", ".*/revive/.*");
-
-    TestConfig::with_filter(runner, filter).spec_id(SpecId::PRAGUE).run().await;
-}
-
-#[tokio::test(flavor = "multi_thread")]
-async fn test_etch_any_contract() {
-    let runner: forge::MultiContractRunner = TEST_DATA_REVIVE.runner_revive();
-    let filter = Filter::new("testEtchAnyContract", "EtchTest", ".*/revive/.*");
+async fn test_etch(#[case] test_name: &str, #[case] runtime_mode: ReviveRuntimeMode) {
+    let runner: forge::MultiContractRunner = TEST_DATA_REVIVE.runner_revive(runtime_mode);
+    let filter = Filter::new(test_name, "EtchTest", ".*/revive/.*");
 
     TestConfig::with_filter(runner, filter).spec_id(SpecId::PRAGUE).run().await;
 }
