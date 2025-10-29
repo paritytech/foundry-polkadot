5b764cacdc01d5da5cd194d28c6bc99e11c3b747
giuseppere <giuseppe.re@parity.io>
2025-10-29 17:18:46 +0100

    Implement thread-safe snapshot getter

--- crates/anvil-polkadot/src/substrate_node/snapshot.rs
+++ crates/anvil-polkadot/src/substrate_node/snapshot.rs
@@ -1,5 +1,6 @@
 use crate::substrate_node::service::{Backend, Client};
-use alloy_primitives::U256;
+use alloy_primitives::{B256, U256};
+use parking_lot::Mutex;
 use polkadot_sdk::{
     polkadot_sdk_frame::runtime::types_common::OpaqueBlock,
     sc_client_api::Backend as BackendT,
@@ -8,6 +9,7 @@ use polkadot_sdk::{
 use std::{collections::BTreeMap, sync::Arc};
 
 type Snapshot = u64;
+type Hash = B256;
 
 pub struct RevertInfo {
     pub info: Info<OpaqueBlock>,
@@ -18,12 +20,17 @@ pub struct SnapshotManager {
     client: Arc<Client>,
     backend: Arc<Backend>,
     next_snapshot_id: U256,
-    snapshots: BTreeMap<U256, Snapshot>,
+    snapshots: Arc<Mutex<BTreeMap<U256, (Snapshot, Hash)>>>,
 }
 
 impl SnapshotManager {
     pub fn new(client: Arc<Client>, backend: Arc<Backend>) -> Self {
-        Self { client, backend, next_snapshot_id: U256::ZERO, snapshots: BTreeMap::new() }
+        Self {
+            client,
+            backend,
+            next_snapshot_id: U256::ZERO,
+            snapshots: Arc::new(Mutex::new(BTreeMap::new())),
+        }
     }
 }
 
@@ -33,13 +40,14 @@ impl SnapshotManager {
         let current_snapshot_id = self.next_snapshot_id;
         self.next_snapshot_id += U256::ONE;
         let snapshot = self.client.info().best_number.into();
-        self.snapshots.insert(current_snapshot_id, snapshot);
+        let hash = self.client.info().best_hash;
+        self.snapshots.lock().insert(current_snapshot_id, (snapshot, hash));
         current_snapshot_id
     }
 
     /// Revert the chain to the block number represented by the snapshot `id`.
     pub fn revert(&mut self, snapshot_id: U256) -> Result<Option<RevertInfo>> {
-        let maybe_snapshot = self.snapshots.remove(&snapshot_id);
+        let (maybe_snapshot, _) = self.snapshots.lock().remove(&snapshot_id);
         let Some(snap) = maybe_snapshot else {
             return Ok(None);
         };
@@ -50,7 +58,7 @@ impl SnapshotManager {
         let (reverted, _) =
             self.backend.revert(number_of_blocks_to_revert.try_into().unwrap_or(u32::MAX), true)?;
 
-        self.snapshots.retain(|_, snap_to_remove| *snap_to_remove < snap);
+        self.snapshots.lock().retain(|_, (snap_to_remove, _)| *snap_to_remove < snap);
 
         Ok(Some(RevertInfo { reverted: reverted.into(), info: self.client.info() }))
     }
@@ -60,5 +68,9 @@ impl SnapshotManager {
         let (reverted, _) =
             self.backend.revert(depth.unwrap_or(1).try_into().unwrap_or(u32::MAX), true)?;
         Ok(RevertInfo { reverted: reverted.into(), info: self.client.info() })
+    }
+
+    pub fn list_snapshots(&self) -> BTreeMap<U256, (u64, B256)> {
+        self.snapshots.lock().clone()
     }
 }
