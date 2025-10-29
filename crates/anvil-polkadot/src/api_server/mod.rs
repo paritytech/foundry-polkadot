79a1e7b1b3b9a330ebdc1a456e420a55046a039e
Alexandru Cihodaru <alexandru.cihodaru@parity.io>
2025-10-29 11:18:09 +0200

    feedback v4
    Signed-off-by: Alexandru Cihodaru <alexandru.cihodaru@parity.io>

--- crates/anvil-polkadot/src/api_server/mod.rs
+++ crates/anvil-polkadot/src/api_server/mod.rs
@@ -35,6 +35,14 @@ pub fn spawn(
     let mut impersonation_manager = ImpersonationManager::default();
     impersonation_manager.set_auto_impersonate_account(config.enable_auto_impersonate);
     let mut signers = config.signer_accounts.clone();
+    signers.extend(
+        config
+            .genesis
+            .iter()
+            .flat_map(|genesis| genesis.alloc.values())
+            .filter_map(|acc| acc.private_key)
+            .filter_map(|k| Keypair::from_secret_key(*k).ok()),
+    );
     if let Some(genesis) = &config.genesis {
         let genesis_signers = genesis
             .alloc
