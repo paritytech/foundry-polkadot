51d8e6c761604e81619ee84b26d42ce20ffa01fb
Sebastian Miasojed <sebastian.miasojed@parity.io>
2025-11-27 10:58:19 +0100

    fix coinbase

--- crates/forge/tests/it/revive/cheats_individual.rs
+++ crates/forge/tests/it/revive/cheats_individual.rs
@@ -64,16 +64,16 @@ revive_cheat_test_original!(test_load, "Load");
 revive_cheat_test_original!(test_addr, "Addr");
 // Not implemented vm.setArbitraryStorage
 // revive_cheat_test_original!(test_arbitrary_storage, "ArbitraryStorage");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test_original!(test_assert, "Assert");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test!(test_assume, "Assume");
 revive_cheat_test_original!(test_assume_no_revert, "AssumeNoRevert");
 // vm.attachBlob vm.broadcast does not work
 revive_cheat_test_original!(test_attach_blob, "AttachBlob");
 // vm.attachDelegation vm.broadcast does not work
 // revive_cheat_test_original!(test_attach_delegation, "AttachDelegation");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test_original!(test_base64, "Base64");
 revive_cheat_test!(test_blob_base_fee, "BlobBaseFee");
 revive_cheat_test_original!(test_blobhashes, "Blobhashes");
@@ -89,11 +89,11 @@ revive_cheat_test_original!(test_clone_account, "CloneAccount");
 revive_cheat_test_original!(test_copy_storage, "CopyStorage");
 //  vm.deployCode not implemented
 revive_cheat_test_original!(test_deploy_code, "DeployCode");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test_original!(test_derive, "Derive");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test_original!(test_ens_namehash, "EnsNamehash");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test_original!(test_env, "Env");
 // EXTCODECOPY compilation issue
 revive_cheat_test_original!(test_etch, "Etch");
@@ -108,32 +108,32 @@ revive_cheat_test_original!(test_get_artifact_path, "GetArtifactPath");
 revive_cheat_test_original!(test_get_chain, "GetChain");
 revive_cheat_test_original!(test_get_code, "GetCode");
 revive_cheat_test_original!(test_get_deployed_code, "GetDeployedCode");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test_original!(test_get_foundry_version, "GetFoundryVersion");
 revive_cheat_test_original!(test_get_label, "GetLabel");
 revive_cheat_test_original!(test_get_nonce, "GetNonce");
 // Implement test to work without fork
 revive_cheat_test!(test_get_raw_block_header, "GetRawBlockHeader");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test_original!(test_json, "Json");
 // revive_cheat_test_original!(test_label, "Label");
 // TODO: check if it is needed
 // revive_cheat_test_original!(test_mapping, "Mapping");
 // TODO: check if it is needed
 // revive_cheat_test_original!(test_mem_safety, "MemSafety");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test_original!(test_parse, "Parse");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test_original!(test_project_root, "ProjectRoot");
 // TODO: check if it is needed
 // revive_cheat_test_original!(test_prompt, "Prompt");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test_original!(test_random_address, "RandomAddress");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test_original!(test_random_bytes, "RandomBytes");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test_original!(test_random_cheatcodes, "RandomCheatcodes");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test_original!(test_random_uint, "RandomUint");
 // TODO: check if it is needed
 revive_cheat_test_original!(test_read_callers, "ReadCallers");
@@ -141,10 +141,10 @@ revive_cheat_test_original!(test_record, "Record");
 revive_cheat_test_original!(test_record_account_accesses, "RecordAccountAccesses");
 revive_cheat_test_original!(test_record_debug_trace, "RecordDebugTrace");
 revive_cheat_test_original!(test_record_logs, "RecordLogs");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test_original!(test_remember, "Remember");
 revive_cheat_test_original!(test_reset_nonce, "ResetNonce");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test_original!(test_rpc_urls, "RpcUrls");
 revive_cheat_test_original!(test_seed, "Seed");
 revive_cheat_test!(test_set_blockhash, "SetBlockhash");
@@ -153,9 +153,9 @@ revive_cheat_test_original!(test_set_nonce, "SetNonce");
 revive_cheat_test_original!(test_set_nonce_unsafe, "SetNonceUnsafe");
 revive_cheat_test_original!(test_setup, "Setup");
 // revive_cheat_test_original!(test_shuffle, "Shuffle");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test_original!(test_sign, "Sign");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test_original!(test_sign_p256, "SignP256");
 revive_cheat_test_original!(test_skip, "Skip");
 // revive_cheat_test_original!(test_sleep, "Sleep");
@@ -163,7 +163,7 @@ revive_cheat_test_original!(test_skip, "Skip");
 revive_cheat_test_original!(test_state_snapshots, "StateSnapshots");
 // Will not work at itt check Gas nd vm.cool
 revive_cheat_test_original!(test_storage_slot_state, "StorageSlotState");
-// SKIP it does not test anything
+// SKIP it should not affect pallet-revive execution
 // revive_cheat_test_original!(test_string_utils, "StringUtils");
 // revive_cheat_test_original!(test_to_string, "ToString");
 // revive_cheat_test_original!(test_toml, "Toml");
