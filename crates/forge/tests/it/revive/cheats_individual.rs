3e29a6125b452f95109e818e237af4c3bd654a13
Pavlo Khrystenko <p.khrystenko@gmail.com>
2025-11-30 19:12:49 +0100

    tests movement

--- crates/forge/tests/it/revive/cheats_individual.rs
+++ crates/forge/tests/it/revive/cheats_individual.rs
@@ -41,3 +41,4 @@ macro_rules! revive_cheat_test_original {
 
 revive_cheat_test!(test_custom_nonce, "Nonce");
 revive_cheat_test_original!(test_nonce, "Nonce");
+revive_cheat_test_original!(test_record_accesses, "RecordAccessesRevive");
