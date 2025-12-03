78b2842d729d600c07d6be279f4d3e69e6903b5b
Pavlo Khrystenko <p.khrystenko@gmail.com>
2025-12-03 16:14:13 +0100

    add expect_call

--- crates/forge/tests/it/revive/cheats_individual.rs
+++ crates/forge/tests/it/revive/cheats_individual.rs
@@ -42,3 +42,4 @@ macro_rules! revive_cheat_test_original {
 revive_cheat_test!(test_custom_nonce, "Nonce");
 revive_cheat_test_original!(test_nonce, "Nonce");
 revive_cheat_test_original!(test_expect_revert, "ExpectRevert");
+revive_cheat_test_original!(test_expect_call, "ExpectCallRevive");
