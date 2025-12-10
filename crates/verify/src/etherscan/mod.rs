cc6de7f38bc5762648033be1c431cfe07d4df99c
Pavlo Khrystenko <p.khrystenko@gmail.com>
2025-12-10 16:15:42 +0100

    upd2

--- crates/verify/src/etherscan/mod.rs
+++ crates/verify/src/etherscan/mod.rs
@@ -520,7 +520,7 @@ mod tests {
 
         let etherscan = EtherscanVerificationProvider::default();
         let client = etherscan.client(&args.etherscan, &args.verifier, &config).unwrap();
-        assert_eq!(client.etherscan_api_url().as_str(), "https://api.etherscan.io/v2/api");
+        assert_eq!(client.etherscan_api_url().as_str(), "https://amoy.polygonscan.com/");
 
         assert!(format!("{client:?}").contains("dummykey"));
 
