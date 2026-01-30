c15324c8984cdb218c0872b624ce2bc3b9c322b7
Pavlo Khrystenko <p.khrystenko@gmail.com>
2026-01-30 17:36:38 +0100

    fixup anvil-polkadot

--- crates/anvil-polkadot/src/substrate_node/genesis.rs
+++ crates/anvil-polkadot/src/substrate_node/genesis.rs
@@ -116,7 +116,13 @@ impl GenesisConfig {
                 let genesis_nonce: u64 = account.nonce.unwrap_or_default();
                 let contract_data: Option<ContractData> = if account.code.is_some() {
                     Some(ContractData {
-                        code: account.code.clone().map(|code| code.to_vec()).unwrap_or_default(),
+                        code: account
+                            .code
+                            .clone()
+                            .map(|code| {
+                                polkadot_sdk::pallet_revive::evm::Bytes::from(code.to_vec())
+                            })
+                            .unwrap_or_default(),
                         storage: account
                             .storage
                             .clone()
