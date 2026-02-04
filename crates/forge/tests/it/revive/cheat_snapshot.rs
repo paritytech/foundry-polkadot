2f5816c73758bd19b7ff44d858f25b0ddd03be7c
Sebastian Miasojed <s.miasojed@gmail.com>
2026-02-04 10:38:03 +0100

    Fix snapshot issues (#512)

--- crates/forge/tests/it/revive/cheat_snapshot.rs
+++ crates/forge/tests/it/revive/cheat_snapshot.rs
@@ -8,9 +8,30 @@ use rstest::rstest;
 #[case::pvm(ReviveRuntimeMode::Pvm)]
 #[case::evm(ReviveRuntimeMode::Evm)]
 #[tokio::test(flavor = "multi_thread")]
-async fn test_snapshot_cheats(#[case] runtime_mode: ReviveRuntimeMode) {
+async fn test_snapshot_state(#[case] runtime_mode: ReviveRuntimeMode) {
     let runner: forge::MultiContractRunner = TEST_DATA_REVIVE.runner_revive(runtime_mode);
     let filter = Filter::new(".*", "StateSnapshotTest", ".*/revive/.*");
+
+    TestConfig::with_filter(runner, filter).spec_id(SpecId::PRAGUE).run().await;
+}
+
+#[rstest]
+#[case::pvm(ReviveRuntimeMode::Pvm)]
+#[case::evm(ReviveRuntimeMode::Evm)]
+#[tokio::test(flavor = "multi_thread")]
+async fn test_snapshot_constructor_contract(#[case] runtime_mode: ReviveRuntimeMode) {
+    let runner: forge::MultiContractRunner = TEST_DATA_REVIVE.runner_revive(runtime_mode);
+    let filter = Filter::new(".*", "SnapshotConstructorContractTest", ".*/revive/.*");
+
+    TestConfig::with_filter(runner, filter).spec_id(SpecId::PRAGUE).run().await;
+}
+
+#[rstest]
+#[case::evm(ReviveRuntimeMode::Evm)]
+#[tokio::test(flavor = "multi_thread")]
+async fn test_snapshot_across_mode_switch(#[case] runtime_mode: ReviveRuntimeMode) {
+    let runner: forge::MultiContractRunner = TEST_DATA_REVIVE.runner_revive(runtime_mode);
+    let filter = Filter::new(".*", "SnapshotAcrossModeSwitchTest", ".*/revive/.*");
 
     TestConfig::with_filter(runner, filter).spec_id(SpecId::PRAGUE).run().await;
 }
