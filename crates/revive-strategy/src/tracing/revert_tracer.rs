1aa0e4782ba19f09742fcb8a9136d3dc25deec61
Sebastian Miasojed <sebastian.miasojed@parity.io>
2026-02-12 09:37:15 +0100

    Address comments

--- crates/revive-strategy/src/tracing/revert_tracer.rs
+++ crates/revive-strategy/src/tracing/revert_tracer.rs
@@ -42,7 +42,7 @@ impl Tracing for RevertTracer {
         &mut self,
         _from: H160,
         to: H160,
-        _is_delegate_call: Option<H160>,
+        is_delegate_call: Option<H160>,
         _is_read_only: bool,
         _value: U256,
         _input: &[u8],
@@ -52,7 +52,7 @@ impl Tracing for RevertTracer {
 
         self.calls.push(if self.call_types.last().is_some_and(|x| matches!(x, Type::Create)) {
             self.current_addr()
-        } else if let Some(delegate) = _is_delegate_call {
+        } else if let Some(delegate) = is_delegate_call {
             delegate
         } else {
             to
