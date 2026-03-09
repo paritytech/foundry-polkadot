761d171799a732a6f49f5ddcfe5c71ed448a6e23
Pavlo Khrystenko <p.khrystenko@gmail.com>
2026-03-09 18:29:07 +0100

    fixup snapshots

--- crates/cast/tests/cli/revive_chain.rs
+++ crates/cast/tests/cli/revive_chain.rs
@@ -3,12 +3,13 @@ use foundry_test_utils::{forgetest_async, revive::AnvilPolkadotNode, util::Outpu
 forgetest_async!(test_cast_chain_id_polkadot_localnode, |_prj, cmd| {
     if let Ok(node) = AnvilPolkadotNode::start().await {
         let rpc_url = node.http_endpoint();
-        cmd.cast_fuse().args(["chain-id", "--rpc-url", &rpc_url]).assert_success().stdout_eq(str![
-            [r#"
-11111111
+        cmd.cast_fuse()
+            .args(["chain-id", "--rpc-url", &rpc_url])
+            .assert_success()
+            .stdout_eq(str![[r#"
+420420420
 
-"#]
-        ]);
+"#]]);
     }
 });
 
