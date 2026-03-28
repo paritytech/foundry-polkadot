f838063d42bf211becc0efdac619e27d499b6f41
filip.parity <filip.baciu@parity.io>
2026-03-28 09:54:01 +0200

    Fix PVM factory deps linking and use rstest for linking tests

--- crates/forge/tests/it/revive/linking.rs
+++ crates/forge/tests/it/revive/linking.rs
@@ -2,24 +2,24 @@ use crate::{config::*, test_helpers::TEST_DATA_REVIVE};
 use foundry_test_utils::Filter;
 use revive_strategy::ReviveRuntimeMode;
 use revm::primitives::hardfork::SpecId;
+use rstest::rstest;
 
+#[rstest]
+#[case::pvm(ReviveRuntimeMode::Pvm)]
+#[case::evm(ReviveRuntimeMode::Evm)]
 #[tokio::test(flavor = "multi_thread")]
-async fn test_revive_deploy_time_linking() {
-    let runner = TEST_DATA_REVIVE.runner_revive(ReviveRuntimeMode::Evm);
+async fn test_revive_deploy_time_linking(#[case] runtime_mode: ReviveRuntimeMode) {
+    let runner = TEST_DATA_REVIVE.runner_revive(runtime_mode);
     let filter = Filter::new("testLibraryLinking", "LinkingTest", ".*/revive/.*");
     TestConfig::with_filter(runner, filter).spec_id(SpecId::SHANGHAI).run().await;
 }
 
+#[rstest]
+#[case::pvm(ReviveRuntimeMode::Pvm)]
+#[case::evm(ReviveRuntimeMode::Evm)]
 #[tokio::test(flavor = "multi_thread")]
-async fn test_revive_factory_deploy_time_linking() {
-    let runner = TEST_DATA_REVIVE.runner_revive(ReviveRuntimeMode::Evm);
+async fn test_revive_factory_deploy_time_linking(#[case] runtime_mode: ReviveRuntimeMode) {
+    let runner = TEST_DATA_REVIVE.runner_revive(runtime_mode);
     let filter = Filter::new("testFactoryLibraryLinking", "FactoryLinkingTest", ".*/revive/.*");
-    TestConfig::with_filter(runner, filter).spec_id(SpecId::SHANGHAI).run().await;
-}
-
-#[tokio::test(flavor = "multi_thread")]
-async fn test_revive_deploy_time_linking_pvm() {
-    let runner = TEST_DATA_REVIVE.runner_revive(ReviveRuntimeMode::Pvm);
-    let filter = Filter::new("testLibraryLinking", "LinkingTest", ".*/revive/.*");
     TestConfig::with_filter(runner, filter).spec_id(SpecId::SHANGHAI).run().await;
 }
--- crates/revive-strategy/src/executor/libraries.rs
+++ crates/revive-strategy/src/executor/libraries.rs
@@ -88,8 +88,49 @@ fn add_missing_dcc_entries(
                 return None;
             }
 
-            let resolc_hash =
-                resolc_artifact.extensions.resolc_extras().and_then(|x| x.hash).unwrap_or_default();
+            let resolc_extras = resolc_artifact.extensions.resolc_extras();
+            let resolc_hash = resolc_extras.as_ref().and_then(|x| x.hash.clone()).unwrap_or_default();
+
+            let mut factory_deps: Vec<BytecodeObject> = resolc_extras
+                .as_ref()
+                .and_then(|x| x.factory_dependencies.as_ref())
+                .map(|deps_map| {
+                    deps_map
+                        .keys()
+                        .filter_map(|hash| {
+                            resolc_output.artifact_ids().find_map(|(_, art)| {
+                                let h = art.extensions.resolc_extras().and_then(|e| e.hash)?;
+                                if &h == hash {
+                                    art.get_bytecode().map(|b| b.object.clone())
+                                } else {
+                                    None
+                                }
+                            })
+                        })
+                        .collect::<Vec<_>>()
+                })
+                .unwrap_or_default();
+
+            if let Some(unlinked_deps) = resolc_extras
+                .as_ref()
+                .and_then(|x| x.factory_dependencies_unlinked.as_ref())
+            {
+                for dep_id in unlinked_deps {
+                    let parts: Vec<&str> = dep_id.rsplitn(2, ':').collect();
+                    if parts.len() == 2 {
+                        let (dep_name, dep_source) = (parts[0], parts[1]);
+                        if let Some(bytecode) = resolc_output.artifact_ids().find_map(|(id, art)| {
+                            if id.name == dep_name && id.source.to_string_lossy().ends_with(dep_source) {
+                                art.get_bytecode().map(|b| b.object.clone())
+                            } else {
+                                None
+                            }
+                        }) {
+                            factory_deps.push(bytecode);
+                        }
+                    }
+                }
+            }
 
             let (evm_bytecode, evm_deployed, evm_hash) = ctx
                 .compilation_output
@@ -129,7 +170,7 @@ fn add_missing_dcc_entries(
                     resolc_bytecode_hash: resolc_hash,
                     resolc_bytecode: resolc_bytecode.clone(),
                     resolc_deployed_bytecode: resolc_bytecode,
-                    resolc_factory_deps: vec![],
+                    resolc_factory_deps: factory_deps,
                     evm_bytecode_hash: evm_hash,
                     evm_bytecode,
                     evm_deployed_bytecode: evm_deployed,
