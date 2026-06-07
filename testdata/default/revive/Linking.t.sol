f7d68c1940b5c0cd4b2952e9c38486574b7cfc16
Pavlo Khrystenko <p.khrystenko@gmail.com>
2026-06-07 22:15:53 +0200

    linking test path fix

--- testdata/default/revive/Linking.t.sol
+++ testdata/default/revive/Linking.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity ^0.8.18;
 
-import "ds-test/test.sol";
+import "utils/Test.sol";
 
 library MathLib {
     function add(uint256 a, uint256 b) public pure returns (uint256) {
