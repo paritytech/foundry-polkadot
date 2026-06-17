a9693f01e5010b49b0320a0fa65df219df565090
Pavlo Khrystenko <p.khrystenko@gmail.com>
2026-06-17 10:38:29 +0200

    more attemps

--- crates/revive-env/src/runtime.rs
+++ crates/revive-env/src/runtime.rs
@@ -93,7 +93,9 @@ impl pallet_balances::Config for Runtime {
     type AccountStore = System;
     type Balance = Balance;
     type ExistentialDeposit = ConstU128<1_000>;
-    type MaxFreezes = ConstU32<1>;
+    type RuntimeFreezeReason = RuntimeFreezeReason;
+    type FreezeIdentifier = RuntimeFreezeReason;
+    type MaxFreezes = frame_support::traits::VariantCountOf<RuntimeFreezeReason>;
 }
 
 #[derive_impl(pallet_timestamp::config_preludes::TestDefaultConfig)]
